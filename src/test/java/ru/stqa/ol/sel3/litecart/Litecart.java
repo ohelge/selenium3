888ce3978c6cf427aaec72fc60ee1a4e107ae2c4
ohelge <otestaa@gmail.com>
2017-02-06 16:52:17 +0100

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки
    ne lokiruet

--- src/test/java/ru/stqa/ol/sel3/litecart/Litecart.java
+++ src/test/java/ru/stqa/ol/sel3/litecart/Litecart.java
@@ -4,6 +4,10 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.javaScriptThrowsNoExceptions;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 /**
@@ -11,19 +15,75 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
  */
 public class Litecart extends TestBase {
 
-  @Test (enabled = false)
+  @Test (enabled = true)
   public void builderAdminLogin() {
     wd.get("http://localhost/litecart/admin/");
-
     wd.findElement(By.name("username")).sendKeys("admin");
     wd.findElement(By.name("password")).sendKeys("admin");
     wd.findElement(By.name("username")).click();
     wd.findElement(By.name("login")).click();
     wait.until(titleIs("My Store"));
+    //Задание 7. Сделайте сценарий, проходящий по всем разделам админки http://software-testing.ru/lms/mod/assign/view.php?id=38588
+    // List<WebElement> table = wd.findElements(By.xpath("//ul[@id='box-apps-menu']"));
+    List<WebElement> rows = wd.findElements(By.xpath("//ul[@id='box-apps-menu']/li[@id='app-']"));
+for (WebElement row : rows)
+{
+  row.findElement(By.xpath("/a")).click();
+}
+    /*wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=appearance&doc=template']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Template')]"));
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=appearance&doc=logotype']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Logotype')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=catalog&doc=catalog']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Catalog')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=countries&doc=countries']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Countries')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=currencies&doc=currencies']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Currencies')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=customers&doc=customers']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Customers')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Geo Zones')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=languages&doc=languages']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Languages')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=modules&doc=jobs']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Job Modules')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=orders&doc=orders']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Orders')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=reports&doc=monthly_sales']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Monthly Sales')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=settings&doc=store_info']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Settings')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=slides&doc=slides']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Slides')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=tax&doc=tax_classes']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Tax Classes')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=translations&doc=search']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Search Translations')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=users&doc=users']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'Users')]"));
+
+    wd.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?app=vqmods&doc=vqmods']")).click();
+    wd.findElement(By.xpath("//h1[contains(text(),'vQmods')]"));*/
+
     wd.manage().deleteAllCookies();
   }
 
-  @Test
+  @Test (enabled = false)
   public void google() {
     wd.get("http://google.com"); //ili wd.navigate().to("http://google.com");
     wd.findElement(By.id("gs_ok0")).click(); //otkrivaem klaviaturu
